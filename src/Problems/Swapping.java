e465c1bd0443a29d5b43567295343fddb2371b2d
imran <imran@LAPTOP-8B4HLJFP>
2024-09-11 18:11:03 +0530

    First commit.

---
+++ src/Problems/Swapping.java
@@ -0,0 +1,16 @@
+package Problems;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=10,b=20;
+		System.out.printf("Before Swappint \na=%d b=%d",a,b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.printf("\nAfter Swappint \na=%d b=%d",a,b);
+
+		
+	}
+
+}
